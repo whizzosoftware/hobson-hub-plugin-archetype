457b8fd4f5a1e0c70e2ee56a7f2c0545efb40da1
Daniel Noguerol <dan@whizzosoftware.com>
2015-06-19 11:02:34 +0200

    Added OSS header to source file templates.

--- src/main/resources/archetype-resources/src/main/java/__pluginName__Plugin.java
+++ src/main/resources/archetype-resources/src/main/java/__pluginName__Plugin.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2015 <Your Company Here>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package ${package};
 
 import java.lang.Override;
